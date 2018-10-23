c19eb77088777efb477f1105a5d5cd798c55e021
simonghrt <simon.gheeraert.sg@gmail.com>
2018-10-23 12:06:46 +0200

    Updates with template and styles in file

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,9 +1,58 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+let style = `
+  .list-messages {
+    display: flex;
+    flex-direction: column;
+    height: 88%;
+    overflow: auto;
+  }
+
+  .left-message {
+    display: flex;
+    margin-left: 10px;
+  }
+
+  .right-message {
+    display: flex;
+    justify-content: flex-end;
+    margin-right: 10px;
+  }
+
+  .input-message {
+    border-top: 2px solid grey;
+    height: 12%;
+  }
+
+  .input-text-message {
+    height: 100%;
+    width: 80%;
+  }
+
+  .input-send {
+    color: #007bff;
+    position: relative;
+    left: 7px;
+    top: -22px;
+  }
+`;
+
 @Component({
   selector: 'chat',
-    templateUrl: './chat.component.html',
-    styleUrls: ['./chat.component.css']
+    template: `
+      <div class="list-messages">
+        <div *ngFor="let message of messages" class="message-container">
+          <message class="message" [text]="message.text" [date]="message.date" [colorBackRight]="colorBackRight" [colorFontRight]="colorFontRight"
+          [colorBackLeft]="colorBackLeft" [colorFontLeft]="colorFontLeft" [owner]="message.userOwner"
+          [ngClass]="{'left-message': !message.userOwner, 'right-message': message.userOwner}"></message>
+        </div>
+      </div>
+      <div class="input-message">
+        <textarea class="input-text-message" [(ngModel)]="textInput" placeholder="Type text here" (keyup)="onKey($event)"></textarea>
+        <i class="fa fa-paper-plane fa-2x input-send" aria-hidden="true" (click)="sendTextInput()"></i>
+      </div>
+    `,
+    styles: [style]
 })
 export class ChatComponent {
 
--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,9 +1,36 @@
 import { Component, Input } from '@angular/core';
 
+let style = `
+  .content-message {
+    width: 80%;
+  }
+  
+  .right-content {
+    text-align: right;
+  }
+  
+  p {
+    display: inline-block;
+    padding: 5px;
+    border-radius: 10px;
+  }  
+`;
+
 @Component({
   selector: 'message',
-    templateUrl: './message.component.html',
-    styleUrls: ['./message.component.css']
+    template: `
+        <div *ngIf="owner" class="content-message right-content">
+            <p [ngStyle]="{'background-color': colorBackRight, 'color': colorFontRight}">
+                {{text}}
+            </p>
+        </div>
+        <div *ngIf="!owner" class="content-message left-content">
+            <p [ngStyle]="{'background-color': colorBackLeft, 'color': colorFontLeft}">
+                {{text}}
+            </p>
+        </div>
+    `,
+    styles: [style]
 })
 export class MessageComponent {
 
